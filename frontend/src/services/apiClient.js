e8f580f98f6427286e6317db80881c4694fb5b15
ashsingh138 <ashking1308@gmail.com>
2026-04-06 20:06:57 +0530

    changes added

--- frontend/src/services/apiClient.js
+++ frontend/src/services/apiClient.js
@@ -1,7 +1,7 @@
 // src/services/apiClient.js
 import axios from 'axios';
 
-const API_BASE_URL = 'http://127.0.0.1:8000/api/v1';
+const API_BASE_URL = 'https://shiprouting.onrender.com/api/v1';
 
 export const optimizeRoute = async (requestData) => {
   try {
