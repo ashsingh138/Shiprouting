a8dbe9922b024b57d831165435a0bef4ebae587d
ashsingh138 <ashking1308@gmail.com>
2026-04-06 20:52:06 +0530

    changes added

--- frontend/src/components/Map/MapView.jsx
+++ frontend/src/components/Map/MapView.jsx
@@ -3,32 +3,42 @@ import React from 'react';
 import { MapContainer, TileLayer } from 'react-leaflet';
 import RouteLayer from './RouteLayer';
 import WeatherLayer from './WeatherLayer';
-// 1. IMPORT THE NEW TRACKER LAYER
 import TrackerLayer from './TrackerLayer'; 
 import ShipLayer from './ShipLayer'; 
 import 'leaflet/dist/leaflet.css';
 
-// 2. ADD trackingData TO THE PROPS
 const MapView = ({ routeData, requestedWaypoints, shipData, trackingData }) => {
-  const center = [10.0, 70.0]; 
+  const center = [20.0, 0.0]; 
+
+  // NEW: Define the extreme corners of the Earth [Latitude, Longitude]
+  const worldBounds = [
+    [-90, -180], // Southwest corner
+    [90, 180]    // Northeast corner
+  ];
 
   return (
-    <MapContainer center={center} zoom={5} className="h-full w-full rounded-md z-0">
+    <MapContainer 
+      center={center} 
+      zoom={3} 
+      
+      // ---------------------------------------------------------
+      // THE FIX: Constrain the Map Viewport
+      // ---------------------------------------------------------
+      minZoom={2}                  // Prevents zooming out into the grey void
+      maxBounds={worldBounds}      // Locks panning to the physical earth
+      maxBoundsViscosity={1.0}     // Makes the world edges a solid wall
+      
+      className="h-full w-full rounded-md z-0"
+    >
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; OpenStreetMap'
+        noWrap={true} // Prevents the map from repeating horizontally
       />
       
       <WeatherLayer routeData={routeData} />
-      
-      {/* Route Planner Layer */}
       <RouteLayer routeData={routeData} requestedWaypoints={requestedWaypoints} />
-      
-      {/* 3. LIVE TRACKING LAYER */}
       {trackingData && <TrackerLayer trackingData={trackingData} />}
-      
-      {/* Note: If TrackerLayer is drawing your ship, you might want to remove ShipLayer so they don't overlap! */}
-      {/* <ShipLayer shipData={shipData} /> */}
       
     </MapContainer>
   );
